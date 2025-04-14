99e5c8b3f6513a1841384b28d8c396a47ed677a9
Александр Комаров <kai___@rambler.ru>
2025-04-14 20:48:56 +0300

    Доработка модели пользователя и регистрация ее в админке. Переход.

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from users.models import FoodGramUser
 
 
 @admin.register(FoodGramUser)
-class FoodGramUserAdmin(admin.ModelAdmin):
+class FoodGramUserAdmin(UserAdmin):
     """Админка для пользователей."""
+
+    search_fields = ('email', 'username')
--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class FoodGramUser(AbstractUser):
     """Модель пользователя."""
+
+    first_name = models.CharField(verbose_name='Имя', max_length=150)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=150)
+    email = models.EmailField(verbose_name='email address', unique=True)
